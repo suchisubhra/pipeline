3c505cc67c60167f15e3a8c03e42f1a939ca84d9
Stephen Lauck <stephen.lauck@gmail.com>
2014-11-03 16:49:23 -0500

    fix FC003

--- recipes/jobs.rb
+++ recipes/jobs.rb
@@ -1,56 +1,60 @@
-
-
 # set up chef-repo job per chef-repo
-search(:chef_orgs, "*:*").each do |org|
-  org['chef_repos'].each do |repo|
+if Chef::Config[:solo]
+  Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
+else
+  search(:chef_orgs, "*:*").each do |org|
+    org['chef_repos'].each do |repo|
 
-    xml = File.join(Chef::Config[:file_cache_path], "#{repo['name']}-config.xml")
+      xml = File.join(Chef::Config[:file_cache_path], "#{repo['name']}-config.xml")
 
-    template xml do
-      source "job-config.xml.erb"
-       variables(
-         :git_url => repo['url'],
-         :build_command => '_knife_commands.sh.erb'
-       )
-    end
+      template xml do
+        source "job-config.xml.erb"
+         variables(
+           :git_url => repo['url'],
+           :build_command => '_knife_commands.sh.erb'
+         )
+      end
 
-    # Create a jenkins job (default action is `:create`)
-    jenkins_job repo['name'] do
-      config xml
-    end
-    
-    begin
-      require 'berkshelf'
-      
-      berksfile = Berkshelf::Berksfile.from_file("#{node['jenkins']['master']['home']}/jobs/#{repo['name']}/workspace/Berksfile")
-      
-      Chef::Log.info("running install on Berksfile...")
+      # Create a jenkins job (default action is `:create`)
+      jenkins_job repo['name'] do
+        config xml
+      end
       
       begin
-        berksfile.update
-      rescue
-        berksfile.install
-      end
-      
-      berksfile.list.reject{|c| c.location == nil}.each do |cookbook|
-        Chef::Log.info(cookbook.location.to_s)
-        xml = File.join(Chef::Config[:file_cache_path], "#{cookbook.name}-config.xml")
-
-        template xml do
-          source "job-config.xml.erb"
-           variables(
-             :git_url => cookbook.location.uri,
-             :build_command => '_cookbook_command.sh.erb'
-           )
+        require 'berkshelf'
+        
+        berksfile = Berkshelf::Berksfile.from_file("#{node['jenkins']['master']['home']}/jobs/#{repo['name']}/workspace/Berksfile")
+        
+        Chef::Log.info("running install on Berksfile...")
+        
+        begin
+          berksfile.update
+        rescue
+          berksfile.install
         end
+        
+        berksfile.list.reject{|c| c.location == nil}.each do |cookbook|
+          Chef::Log.info(cookbook.location.to_s)
+          xml = File.join(Chef::Config[:file_cache_path], "#{cookbook.name}-config.xml")
 
-        # Create a jenkins job (default action is `:create`)
-        jenkins_job cookbook.name do
-          config xml
+          template xml do
+            source "job-config.xml.erb"
+             variables(
+               :git_url => cookbook.location.uri,
+               :build_command => '_cookbook_command.sh.erb'
+             )
+          end
+
+          # Create a jenkins job (default action is `:create`)
+          jenkins_job cookbook.name do
+            config xml
+          end
         end
+      rescue Exception => e
+        Chef::Log.error("Error reading Berksfile: #{e.message}")
       end
-    rescue Exception => e
-      Chef::Log.error("Error reading Berksfile: #{e.message}")
     end
   end
 end
+
+
--- recipes/knife.rb
+++ recipes/knife.rb
@@ -25,28 +25,27 @@ end
 
 # search data bag chef_orgs for each chef server or org
 # write out knife and pem
-search(:chef_orgs, "*:*").each do |org|
-  template "#{node['jenkins']['master']['home']}/.chef/knife.rb" do
-    cookbook 'pipeline'
-    source "knife.rb.erb"
-    owner node['jenkins']['master']['user']
-    group node['jenkins']['master']['group']
-    mode 0644
-    variables(
-      :chef_server_url => org['chef_server_url'],
-      :client_node_name => org['client']
-    )
+if Chef::Config[:solo]
+  Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
+else
+  search(:chef_orgs, "*:*").each do |org|
+    template "#{node['jenkins']['master']['home']}/.chef/knife.rb" do
+      cookbook 'pipeline'
+      source "knife.rb.erb"
+      owner node['jenkins']['master']['user']
+      group node['jenkins']['master']['group']
+      mode 0644
+      variables(
+        :chef_server_url => org['chef_server_url'],
+        :client_node_name => org['client']
+      )
+    end
+
+    file "#{node['jenkins']['master']['home']}/.chef/#{org['client']}.pem" do
+      content org['pem']
+      owner node['jenkins']['master']['user']
+      group node['jenkins']['master']['group']
+      mode 0644
+    end   
   end
-
-  file "#{node['jenkins']['master']['home']}/.chef/#{org['client']}.pem" do
-    content org['pem']
-    owner node['jenkins']['master']['user']
-    group node['jenkins']['master']['group']
-    mode 0644
-  end   
 end
-
-
-
-
-

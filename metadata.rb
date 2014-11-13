82a76096a62808e0548d0993392c7758cbbeab46
Stephen Lauck <stephen.lauck@gmail.com>
2014-11-13 13:22:06 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "lauck@getchef.com"
 license          'All rights reserved'
 description      'Installs/Configures a Jenkins based chef delivery pipeline'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.1.0'
+version          '2.1.1'
 
 %w{ apt yum git java jenkins chef-zero emacs sudo chef-dk resolver }.each do |cb|
   depends cb

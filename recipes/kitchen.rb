78879c4c8fcaedd379033d4ef901ebada4824d56
Stephen Lauck <stephen.lauck@gmail.com>
2014-11-25 15:20:16 -0500

    add config.yml and install gem via chef

--- recipes/kitchen.rb
+++ recipes/kitchen.rb
@@ -1,6 +1,17 @@
 include_recipe "build-essential"
 
-chef_gem "kitchen-ec2" do
-  action :install
-end
+# chef_gem "kitchen-ec2" do
+#   action :install
+# end
 
+execute "chef gem install kitchen-ec2"
+
+file "#{node['jenkins']['master']['home']}/.kitchen/config.yml" do
+  content <<-EOD
+---
+driver:
+  name: digitalocean
+  EOD
+  owner node['jenkins']['master']['user']
+  group node['jenkins']['master']['user']
+end
